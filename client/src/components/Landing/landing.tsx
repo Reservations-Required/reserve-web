2870fd594700cc43de9b5472bdeb865ac0403c10
Kevin L <kevinlin8221@gmail.com>
2022-05-31 12:39:53 -0400

    mapped list of buildings to buttons

--- client/src/components/Landing/landing.tsx
+++ client/src/components/Landing/landing.tsx
@@ -3,18 +3,13 @@ import { useState } from 'react';
 import './landing.css'
 
 const Landing = () => {
+    const buildings: string[] = ["Morrison", "Olin", "Upson", "Uris", "Carpenter", "Rhodes"];
+    const buildingsMap: React.ReactNode = buildings.map((e) => { return <Button> {e} </Button> })
     return (
         <div>
-            {/* const buildings = ["Morrison","Olin","Upson","Uris","Carpenter","Rhodes"]; */}
             <p> Reservation Required </p>
             <div className="center"><h1> Popular Locations </h1>
-                {/* buildings.map((e) => {<Button> {e} </Button>}) */}
-                <Button> Morrison </Button>
-                <Button> Olin </Button>
-                <Button> Upson </Button>
-                <Button> Uris </Button>
-                <Button> Carpenter </Button>
-                <Button> Rhodes </Button>
+                {buildingsMap}
             </div>
         </div>
     );
