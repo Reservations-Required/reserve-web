609b5bd1358f9cae020f9887b5a3b216ba057d0f
Dylan Tom <dt425@cornell.edu>
2022-05-31 19:31:06 -0400

    fixed typing for landing

--- client/src/components/Landing/landing.tsx
+++ client/src/components/Landing/landing.tsx
@@ -4,7 +4,7 @@ import './landing.css'
 
 const Landing = () => {
     const buildings: string[] = ["Morrison", "Olin", "Upson", "Uris", "Carpenter", "Rhodes"];
-    const buildingsMap: React.ReactNode = buildings.map((e) => { return <Button> {e} </Button> })
+    const buildingsMap: React.ReactNode[] = buildings.map((e) => { return <Button> {e} </Button> })
     return (
         <div>
             <p> Reservation Required </p>
