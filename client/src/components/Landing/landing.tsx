9ad9690af612a6b644cc91bca38ee8f6a7947337
Kevin L <kevinlin8221@gmail.com>
2022-06-04 00:25:49 -0400

    Updated Header/Buttons

--- client/src/components/Landing/landing.tsx
+++ client/src/components/Landing/landing.tsx
@@ -3,13 +3,13 @@ import { useState } from 'react';
 import './landing.css'
 
 const Landing = () => {
-    const buildings: string[] = ["Morrison", "Olin", "Upson", "Uris", "Carpenter", "Rhodes"];
-    const buildingsMap: React.ReactNode[] = buildings.map((e) => { return <Button> {e} </Button> })
+    const buildings: string[] = ["Morrison 218", "Morrison 220", "Morrison 222", "Morrison 224"];
+    const buildingsMap: React.ReactNode = buildings.map((e) => { return <Button className = "buttons"> {e} </Button>})
     return (
         <div>
             <p> Reservation Required </p>
-            <div className="center"><h1> Popular Locations </h1>
-                {buildingsMap}
+            <div className = "center"><h1> Suggestions </h1>
+            {buildingsMap}
             </div>
         </div>
     );
