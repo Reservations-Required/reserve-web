f148c88ae2aa7ff8e54945212a9feda27a717aa3
vanessafang3 <vanessa.fang3@gmail.com>
2022-06-09 17:43:41 -0400

    routing is complete!

--- client/src/components/Home/home.tsx
+++ client/src/components/Home/home.tsx
@@ -1,28 +1,30 @@
 import './home.css';
-// import React from 'react';
-// import { Button } from '@mui/material';
-// import Landing from '../Landing/landing';
-// import Account from '../Account/account';
+import React from 'react';
+import { Button } from '@mui/material';
+import Landing from '../Landing/landing';
+import Account from '../Account/account';
 
 
 const Home = () => {
-  // const SERVER_URL = "http://localhost:8080/api";
+  const SERVER_URL = "http://localhost:8080/api";
 
-  // async function fetchBuilding() {
-  //   const res = await fetch(`${SERVER_URL}`, {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       'Accept': 'application/json'
-  //     }
-  //   });
+  async function fetchBuilding() {
+    const res = await fetch(`${SERVER_URL}`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    });
 
-  //   const building = await res.json();
-  //   console.log(building);
-  // }
+    const building = await res.json();
+    console.log(building);
+  }
 
   return (
     <div className="Home">
-      <p>Hi!</p>
+      <Button onClick={fetchBuilding}>Click to Fetch</Button>
+      <Landing />
+      <Account name = {"Clara Lee"} email = {"cl123@ebnet.org"} />
     </div>
   );
 }
