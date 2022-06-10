397abbc7ba6e7488cbc9efab7369ac38c3365efe
Dylan Tom <dylantom222@gmail.com>
2022-06-10 17:26:31 -0400

    Added functionality for backend firebase

--- server/src/router/buildings.ts
+++ server/src/router/buildings.ts
@@ -1,10 +1,30 @@
 import express from 'express';
-import { db } from '../firebase.js';
+import { db } from '../firebase';
 
 const router = express.Router();
 
-router.get("/:b_id", (req, res) => {
-	res.json({ message: `Returned info about building ${req.params.b_id}!` })
+/***
+ * Returns information about each building
+ */
+router.get("/", async (req, res) => {
+	const buildingsCollection = db.collection('Buildings');
+	const buildingsSnapshot = await buildingsCollection.get();
+	const allBuildings = buildingsSnapshot.docs;
+	const buildings = [];
+	for (const doc of allBuildings) {
+		const building = doc.data();
+		buildings.push(building);
+	}
+
+	res.send(buildings);
+});
+
+router.get("/:b_id", async (req, res) => {
+	const buildingID = req.params.b_id;
+	const buildingsCollection = db.collection('Buildings');
+	const ref = buildingsCollection.doc(buildingID);
+	const doc = await ref.get();
+	res.send(doc);
 });
 
 router.put("/:b_id/buildings/1", (req, res) => {
