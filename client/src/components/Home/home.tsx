1ae595be97cae007998b02ca9561d590dfaad5dd
Dylan Tom <dylantom222@gmail.com>
2022-06-12 12:34:48 -0400

    updated buildings access and fetch function

--- client/src/components/Home/home.tsx
+++ client/src/components/Home/home.tsx
@@ -1,4 +1,5 @@
 import './home.css';
+import {useState} from 'react';
 import { Button } from '@mui/material';
 import Landing from '../Landing/landing';
 import RoomCard from '../RoomCard/roomcard';
@@ -6,8 +7,8 @@ import RoomCard from '../RoomCard/roomcard';
 const Home = () => {
   const SERVER_URL = "http://localhost:8080/api";
 
-  async function fetchBuilding() {
-    const res = await fetch(`${SERVER_URL}`, {
+  async function fetchBuilding(x: number) {
+    const res = await fetch(`${SERVER_URL}/buildings/${x}`, {
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
@@ -20,7 +21,7 @@ const Home = () => {
 
   return (
     <div className="Home">
-      <Button onClick={fetchBuilding}>Click to Fetch</Button>
+      <Button onClick={() => {fetchBuilding(2)}}>Click to Fetch</Button>
       <Landing />
       <RoomCard building='Morrison' room={218} people={['5','7']} favorite={false}/>
     </div>
--- server/src/router/buildings.ts
+++ server/src/router/buildings.ts
@@ -7,7 +7,7 @@ const router = express.Router();
  * Returns information about each building
  */
 router.get("/", async (req, res) => {
-	const buildingsCollection = db.collection('Buildings');
+	const buildingsCollection = db.collection('buildings');
 	const buildingsSnapshot = await buildingsCollection.get();
 	const allBuildings = buildingsSnapshot.docs;
 	const buildings: FirebaseFirestore.DocumentData[] = [];
@@ -19,12 +19,18 @@ router.get("/", async (req, res) => {
 	res.send(buildings);
 });
 
+/***
+ * Returns information about a specific building, given its 
+ * building ID
+ */
 router.get("/:b_id", async (req, res) => {
 	const buildingID = req.params.b_id;
-	const buildingsCollection = db.collection('Buildings');
+	const buildingsCollection = db.collection('buildings');
 	const ref = buildingsCollection.doc(buildingID);
 	const doc = await ref.get();
-	res.send(doc);
+	const data = doc.data();
+	
+	res.send(data);
 });
 
 router.put("/:b_id/buildings/1", (req, res) => {
