87bd3e29320714e2ed3d79d5b4d64da48ad7d0cb
Tony Chen <tonychen7878@gmail.com>
2022-07-02 21:44:23 -0400

    initial version of sign in page completed

--- client/src/components/Login/login.tsx
+++ client/src/components/Login/login.tsx
@@ -4,6 +4,10 @@ import { auth, signInWithGoogle } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import "./login.css";
 
+import googleLogo from "../../assets/google_logo.svg"
+import loginLogo from "../../assets/login_logo.svg"
+import {H1, H4, P1, P3} from "../../styles/fonts.style"
+
 const Login = () => {
   const [user, loading] = useAuthState(auth);
   const navigate = useNavigate();
@@ -14,13 +18,29 @@ const Login = () => {
     }
     if (user) navigate("/profile");
   }, [user, loading]);
+
   return (
     <div className="login">
-      <div className="login__container">
-        <button className="login__btn login__google" onClick={signInWithGoogle}>
-          Login with Google
-        </button>
+      <div className="login_logo">
+        <img src={loginLogo}></img>
       </div>
+      <div className="login_title">
+        <div className="login_title1">
+          <H4>Welcome to</H4>
+        </div>
+        <div className="login_title2">
+          <H1>Reservations</H1>
+          <H1>Required.</H1>
+        </div>
+      </div>
+      <div className="login_text">
+        <P1>First time and returning users...</P1>
+        <P1>sign in below!</P1>
+      </div>
+      <button className="login_button" onClick={signInWithGoogle}>
+        <img className="google_logo" src={googleLogo}></img>
+        <P3>Sign in with Google</P3>
+      </button>
     </div>
   );
 }
