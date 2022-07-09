06fdcebdd8156a1618c9e6d1ea22ab7c45a98bb9
Dylan Tom <tomd@bxscience.edu>
2022-07-09 09:46:48 -0400

    adjusted for React 17 deprecation in index.tsx

--- client/src/components/Profile/profile.tsx
+++ client/src/components/Profile/profile.tsx
@@ -3,9 +3,9 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import "./profile.css";
 import { auth, db, logout } from "../../firebase";
-import Account from "../Confirmation/Account/account";
 import { StyledButton } from "../../styles/button.style";
 import { fetchUserName } from "../../firebase/functions";
+import { H2, P1, P5 } from "../../styles/fonts.style";
 
 const Profile = () => {
   const [user, loading] = useAuthState(auth);
@@ -20,9 +20,9 @@ const Profile = () => {
 
   return (
     <div className="dashboard">
-      <div className="account">
-        <Account name={name} email={user?.email} />
-      </div>
+      <H2>Hi, {name.split(" ")[0]}!</H2>
+      <P5>Here are all your reservations and favorited rooms.</P5>
+      <P1>Favorited Rooms</P1>
       <div className="button">
         <StyledButton onClick={logout}>Logout</StyledButton>
       </div>
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
+/*** Added support for React 18 */
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
