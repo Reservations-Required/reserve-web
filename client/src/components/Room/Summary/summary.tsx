027b97375ede622f0238c247c3a2702fff7b4223
vanessafang3 <vanessa.fang3@gmail.com>
2022-07-12 21:00:11 -0400

    summary card updated
    similar to high fi summary card

--- client/src/components/Room/Summary/summary.tsx
+++ client/src/components/Room/Summary/summary.tsx
@@ -5,6 +5,7 @@ import DateIcon from '../../../assets/date.svg';
 import { H3, P1, P3, P6, P7 } from '../../../styles/fonts.style';
 import { StyledButton2 } from '../../../styles/button.style';
 import { useState } from 'react';
+import PencilIcon from '../../../assets/pencilIcon.svg';
 
 interface SummaryProps {
 	building: string;
@@ -15,29 +16,43 @@ const Summary = (props: SummaryProps) => {
 	return (
 		<div className="summary">
 			<P1 className="header">Reservation Summary</P1>
-			<Grid container columns={2}>
-				<Grid item xs={1}>
+			<div className='grid1'>
+				
 					<P6>Reservation Room </P6>
-				</Grid>
-				<Grid item xs={1}>
 					<P3>{props.building} {props.room}</P3>
+				
+			</div>
+
+			<div className='grid2'>
+				<Grid className='sum2'>
+					<div className='sum2-longcol1'>
+						<img src={DateIcon} />
+					</div>
+
+					<div className='sum2-longrow'>
+						<P3>Wednesday, April 20, 2022</P3>
+					</div>
+
+					<div className='sum2-longcol2'>
+						<img src={PencilIcon} />
+					</div>
+
+					<div className='sum2-longrow'>
+						<P3>11:00 AM - 12:00 PM</P3>
+					</div>
 				</Grid>
-			</Grid>
-			<Grid container columns={2}>
-				<Grid item xs = {1}>
-					<img src={DateIcon} />
-				</Grid>
-				<Grid item xs={1}>
-					<P3>Wednesday, April 20, 2022</P3>
-				</Grid>
-				<Grid item xs = {1}/>
-				<Grid item xs={1}>
-					<P3>11:00 AM - 12:00 PM</P3>
-				</Grid>
-			</Grid>
-			<P6>Purpose</P6>
-			<TextField variant="standard" label="Why are you reserving this room? (optional)" />
-			<StyledButton2 className="reserve_btn">Reserve This Room</StyledButton2>
+			</div>
+
+			<div className='sum-purpose'>
+				<P6 className='p-text'>Purpose</P6>
+				<TextField className='purpose-text'
+					variant="standard"
+					placeholder="Why are you reserving this room? (optional)" />
+				<div className='space'></div>
+				<div className="reserve_btn">
+					<P1 className='rnow'>Reserve This Room</P1>
+				</div>
+			</div>
 		</div>
 	)
 }
