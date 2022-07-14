d757a3f62d461c69fec08a32bda37c44b3d6b2c0
Kevin L <kevinlin8221@gmail.com>
2022-07-13 22:34:27 -0400

    added css
    separated the buildings and campus into two sections

--- client/src/components/BuildingSearch/BuildingSearch.tsx
+++ client/src/components/BuildingSearch/BuildingSearch.tsx
@@ -1,31 +1,37 @@
 import { P4, P1 } from '../../styles/fonts.style';
 import BuildingIcon from '../../assets/BuildingIcon.svg';
 import TreeIcon from '../../assets/TreeIcon.svg';
+import { BuildingSearchButton } from '../../styles/button.style';
+import './BuildingSearch.css';
+
+
 const BuildingSearch = () => {
+    const campus: string[] = ["North Campus", "Engineering Quad", "Arts Quad", "Agriculture Quad"];
+    const campusMap: React.ReactNode[] = campus.map((e) => { return <BuildingSearchButton> {e} </BuildingSearchButton> });
+
+    const buildings: string[] = ["Toni Morrison Hall", "Ruth Bader Ginsburg Hall", "Barbara McClintock Hall", "Upson Hall", "Carpenter Hall", "Olin Library"];
+    const buildingsMap: React.ReactNode[] = buildings.map((e) => { return <BuildingSearchButton> {e} </BuildingSearchButton> });
     return (
-        <div>
-            <P1>
-                <img src={TreeIcon} />
-                Campus
-            </P1>
-            <P4>
-                North Campus
-                Engineering Quad
-                Arts Quad
-                Agriculture Quad
-            </P4>
-            <P1>
-                <img src={BuildingIcon} />
-                Building
-            </P1>
-            <P4>
-                Toni Morrison Hall
-                Ruth Bader Ginsburg Hall
-                Barbara McClintock Hall
-                Upson Hall
-
-            </P4>
-
+        <div className= "heading">
+            <div className = "campus">
+                <P1>
+                    <img src={TreeIcon} />
+                    Campus
+                </P1>
+                <P4>
+                    {campusMap}
+                </P4>
+            </div>
+            <div className="vl"></div>
+            <div className = "buildings">
+                <P1>
+                    <img src={BuildingIcon} />
+                    Building
+                </P1>
+                <P4>
+                    {buildingsMap}
+                </P4>
+            </div>
         </div>
     );
 };
